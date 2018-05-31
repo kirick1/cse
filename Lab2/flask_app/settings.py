4ba1928cc753db385c9ac14f135ad75e4f144c15
vlkr <kyrychenko.vladyslav@gmail.com>
2018-06-01 02:28:10 +0300

    fix

--- Lab2/flask_app/settings.py
+++ Lab2/flask_app/settings.py
@@ -1,9 +1,4 @@
-from os.path import dirname, join
-from dotenv import get_key
-
-dotenv_path = join(dirname(__file__), '.env')
-
 ITEM_PIPELINES = {
     'app.spider.pipelines.MongoPipeline': 300,
 }
-MONGO_URI = get_key(dotenv_path, 'MONGO_URI')
+MONGO_URI = 'mongodb://laber:1234567890@ds119070.mlab.com:19070/python-lab-2'
