59d0810a5671e4172f05bf931105f6c18948b91e
vlkr <kyrychenko.vladyslav@gmail.com>
2018-06-04 14:15:21 +0300

    final

--- Lab2/flask_app/run.py
+++ Lab2/flask_app/run.py
@@ -4,6 +4,6 @@ import os
 
 if __name__ == '__main__':
     serve(app,
-          host=os.environ.get("HOST", "0.0.0.0"),
-          port=os.environ.get("PORT", 3000),
+          host=os.environ.get("HOST", "localhost"),
+          port=os.environ.get("PORT", 5000),
           expose_tracebacks=True)
